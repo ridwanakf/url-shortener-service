bb35c9593996f074acfe46812511b2065e3bcba3
ridwanakf <blog.afwan@gmail.com>
2020-06-26 12:20:43 +0700

    Update repo and usecase contracts

--- internal/repo.go
+++ internal/repo.go
@@ -7,8 +7,8 @@ import (
 // ShortenerDBRepo contains repo for all URL Shortener DB
 //go:generate mockgen -destination=repo/db/url_shortener_db_mock.go -package=db github.com/ridwanakf/url-shortener-service/internal ShortenerDBRepo
 type ShortenerDBRepo interface {
-	GetAllURL() ([]entity.URL, error)
-	GetLongURL(shortURL string) (string, error)
+	GetAllURL(userID string) ([]entity.URL, error)
+	GetURL(shortURL string) (entity.URL, error)
 	CreateNewShortURL(url entity.URL) error
 	UpdateShortURL(shortURL string, longURL string) error
 	DeleteURL(shortURL string) error
@@ -20,7 +20,11 @@ type ShortenerDBRepo interface {
 // ShortenerCacheRepo contains repo for all URL Shortener Cache
 //go:generate mockgen -destination=repo/redis_cache/url_shortener_redis_mock.go -package=redis_cache github.com/ridwanakf/url-shortener-service/internal ShortenerCacheRepo
 type ShortenerCacheRepo interface {
-	GetLongURL(shortURL string) (string, error)
-	IsShortURLExist(shortURL string) (bool, error)
-	DeleteURL(shortURL string) (bool, error)
+	GetAllURL(userID string) ([]entity.URL, error)
+	GetURL(shortURL string) (string, error)
+	SetURL(url entity.URL) error
+	DeleteURL(shortURL string) (int64, error)
+	IsSingleURLExist(shortURL string) (bool, error)
+	HasShortURLExpired(shortURL string) (bool, error)
+	IsCollectionURLExist(userID string) (bool, error)
 }
